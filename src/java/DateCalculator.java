88d51bab6c5be2b8d94db89173c86cfa40b5f7e9
aktana-joey <joey.yang@aktana.com>
2021-07-26 18:28:47 +0800

    change to static

--- src/java/DateCalculator.java
+++ src/java/DateCalculator.java
@@ -3,22 +3,11 @@ import java.util.Map;
 
 public class DateCalculator {
 
+    private static Map<Integer,Integer> monthAndDays= new HashMap<>();
 
-    public Integer calculateDaysOfYear(int year, int month, int day){
-        if(!checkDateValid(year,month,day)){
-            return -1;
-        }
-
-        Map<Integer,Integer> monthAndDays= new HashMap<>();
-        Integer daysOfYear = day;
+    static{
         //Put month & days of month to a map
         monthAndDays.put(1,31);
-        //If the year is leap year, the days of Feb is 29.
-        if (isLeapYear(year)) {
-            monthAndDays.put(2,29);
-        }else{
-            monthAndDays.put(2,28);
-        }
         monthAndDays.put(3,31);
         monthAndDays.put(4,30);
         monthAndDays.put(5,31);
@@ -29,6 +18,20 @@ public class DateCalculator {
         monthAndDays.put(10,31);
         monthAndDays.put(11,30);
         monthAndDays.put(12,31);
+    }
+
+
+    public static Integer calculateDaysOfYear(int year, int month, int day){
+        if(!checkDateValid(year,month,day)){
+            return -1;
+        }
+        Integer daysOfYear = day;
+        //If the year is leap year, the days of Feb is 29.
+        if (isLeapYear(year)) {
+            monthAndDays.put(2,29);
+        }else{
+            monthAndDays.put(2,28);
+        }
 
         for(Map.Entry<Integer,Integer> entry : monthAndDays.entrySet()){
             //If the month < input month, add to days of month
@@ -39,11 +42,11 @@ public class DateCalculator {
         return daysOfYear;
     }
 
-    private Boolean isLeapYear(int year){
+    private static Boolean isLeapYear(int year){
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
-    private Boolean checkDateValid(int year,int month,int day){
+    private static Boolean checkDateValid(int year, int month, int day){
 
         if(year >= 1970 && month <= 12 && month > 0 && day>0 && day<=31){
             return true;
--- src/test/Tester.java
+++ src/test/Tester.java
@@ -22,19 +22,19 @@ public class Tester {
         Integer month = calendar.get(Calendar.MONTH);
         Integer day = calendar.get(Calendar.DATE);
 
-        DateCalculator calculator = new DateCalculator();
+
         //check if the calculator works fine
-        Assert.check(-1 == calculator.calculateDaysOfYear(1960,2,28));
-        Assert.check(-1 == calculator.calculateDaysOfYear(1970,13,31));
-        Assert.check(-1 == calculator.calculateDaysOfYear(1970,12,32));
-        Assert.check(28+31 == calculator.calculateDaysOfYear(2021,2,28));
+        Assert.check(-1 == DateCalculator.calculateDaysOfYear(1960,2,28));
+        Assert.check(-1 == DateCalculator.calculateDaysOfYear(1970,13,31));
+        Assert.check(-1 == DateCalculator.calculateDaysOfYear(1970,12,32));
+        Assert.check(28+31 == DateCalculator.calculateDaysOfYear(2021,2,28));
 
 
         System.out.println("---------------Input Year : " + year);
         System.out.println("---------------Input Month : " + month);
         System.out.println("---------------Input Day : " + day);
 
-        Integer daysOfYear = calculator.calculateDaysOfYear(year,month,day);
+        Integer daysOfYear = DateCalculator.calculateDaysOfYear(year,month,day);
 
         System.out.println("---------------Days Of Year : " +daysOfYear);
 
